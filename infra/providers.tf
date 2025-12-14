c061833971dd4ad9d8aa1dd9cfe80c0cc88a5898
MarceloACJunior <marcelinhoalcantaracruz@gmail.com>
2025-12-13 23:15:21 -0300

    feat: adicionando subida de listas de queues

---
+++ infra/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.14.1"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "terraform-state-bucket-nextime"
+    key     = "sqs/infra.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
